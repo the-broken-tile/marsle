6e45cfaff615c5bec6ab1dd662203c66e531ab0a
Rusi Papazov <rusi.papazov@jiminny.com>
2024-05-17 21:42:34 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+### To run
+```shell
+webpack --watch
+```
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+
+module.exports = {
+    entry: "./src/index.ts",
+    mode: "development",
+    devtool: "inline-source-map",
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: "ts-loader",
+                exclude: /node_modules/,
+            }, {
+                test: /\.json$/,
+                type: "javascript/auto",
+                use: "json-loader",
+                exclude: /node_modules/,
+            }, {
+                test: /\.css$/,
+                use: ["css-loader"],
+            },
+        ],
+    },
+    resolve: {
+        extensions:  [".ts", ".tsx", ".js", ".jsx"],
+        fallback: {
+            crypto: false,
+        },
+    },
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist"),
+    },
+    devServer: {
+        contentBase: "./dist",
+    },
+};
