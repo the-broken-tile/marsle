3218439c6c797cc8ca5768b9074374331325bd6c
Rusi Papazov <rusi.papazov@jiminny.com>
2024-05-19 00:52:45 +0300

    remove dev mode from webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,6 @@ const path = require("path");
 
 module.exports = {
     entry: "./src/index.ts",
-    mode: "development",
-    devtool: "inline-source-map",
     module: {
         rules: [
             {
