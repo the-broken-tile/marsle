525e9a55c44b72a980401a06aba37c68c20279bb
Rusi Papazov <rusi.papazov@jiminny.com>
2024-05-20 15:10:04 +0300

    add up/down to vp

--- src/Component/Match.ts
+++ src/Component/Match.ts
@@ -12,7 +12,7 @@ export default class Match {
     }
 
     public toString(): string {
-        return `<span class="guess guess-${this.match.type} ${this.getClassName()}">${this.innerHtml()}</span>`
+        return `<span class="guess guess-${this.match.type} match-${this.match.value}">${this.innerHtml()}</span>`
     }
 
     private innerHtml(): string {
@@ -20,7 +20,7 @@ export default class Match {
             case MatchType.name:
                 return this.match.target.name
             case MatchType.cost:
-                return this.renderCost()
+                return this.match.target.cost.toString()
             case MatchType.expansion:
                 return new Expansion(this.match.target.expansion).toString()
             case MatchType.type:
@@ -32,30 +32,6 @@ export default class Match {
             case MatchType.vp:
                 return this.renderVP()
         }
-    }
-
-    private renderCost(): string {
-        if (this.match.value === MatchValue.full) {
-            return this.match.target.cost.toString()
-        }
-
-        return `${this.match.target.cost.toString()} ${this.match.value === MatchValue.higher ? "👆" : "👇"}`
-    }
-
-    private getClassName(): string {
-        if (this.match.type === MatchType.name) {
-            return this.match.value === MatchValue.full ? "full-match" : ""
-        }
-
-        if (this.match.value === MatchValue.full) {
-            return "full-match"
-        }
-
-        if (this.match.value === MatchValue.no) {
-            return "no-match"
-        }
-
-        return "partial-match"
     }
 
     private renderVP(): string {
